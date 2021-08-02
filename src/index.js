ffb8b18161b85fd87b8db19cfd1f082befb4a141
msmarketa <marketa.dragounova@gmail.com>
2021-08-02 15:13:54 +0200

    functioning timer

--- src/index.js
+++ src/index.js
@@ -45,35 +45,43 @@ const resetBtn = document.getElementById("reset-btn");
 timer.value = 10;
 countdown.value = `${timer.value}:00`;
 
-timer.oninput = () => {
-  const totalSec = timer.value * 60;
+let totalSec = timer.value * 60;
+let counterId;
+
+timer.addEventListener('input', () => {
+  totalSec = timer.value * 60;
+  updateValue();
+});
+
+function updateValue() {
   countdown.value = `${Math.floor(totalSec / 60)}:${(totalSec % 60).toString().padStart(2, "0")}`;
 }
 
-// Kliknu na start
-//   zavolam startTimer() ktery ma setInterval(decrementTimer, 1000)
-//   decrementTimer meni countdown.value -- vterinu
+function decreaseValue() {
+  if (totalSec >= 0) {
+    updateValue();
+    totalSec--;
+  } else {
+    stopTimer();
+  }
+}
 
 function runTimer() {
-  setInterval(() => {
-    
-    for (let minutes = timer.value; minutes >= 0; minutes--) {
-      countdown.value = `${minutes}:00`;
-    }
-  }, 1000)
+  counterId = setInterval(decreaseValue, 1000);
 }
 
 const startTimer = () => {
-  console.log("Starting!");
   runTimer();
 }
 
 const stopTimer = () => {
-  console.log("Stopping")
+  clearInterval(counterId);
 }
 
 const resetTimer = () => {
-  console.log("Resetting")
+  stopTimer();
+  totalSec = timer.value * 60;
+  decreaseValue();
 }
 
 startBtn.addEventListener("click", startTimer);
